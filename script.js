339a5024d9d64623a67babc30fc72b44d12aabdc
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2024-11-26 20:23:27 -0300

    carroussel

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let index = 0; // Índice da imagem atual
-const items = document.querySelectorAll('.carrossel-item'); // Seleciona todos os itens do carrossel
+const items = document.querySelectorAll('.carrossel-item-videoclip'); // Seleciona todos os itens do carrossel
 const totalItems = items.length; // Total de imagens no carrossel
+
+let index1 = 0;
+const items1 = document.querySelectorAll('.carrossel-item-player');
+const totalItems1 = items1.length; 
 
 // Função para mudar a imagem
 function mudarImagem(direcao) {
@@ -8,6 +12,15 @@ function mudarImagem(direcao) {
   index = (index + direcao + totalItems) % totalItems; // Garante que o índice não saia do intervalo
 
   // Move o carrossel para a posição da imagem desejada
-  const container = document.querySelector('.carrossel-container');
+  const container = document.querySelector('.carrossel-container-videoclip');
   container.style.transform = `translateX(-${index * 100}%)`;
 }
+
+function mudarImagem1(direcao) {
+
+  index1 = (index1 + direcao + totalItems1) % totalItems1;
+
+  const container = document.querySelector('.carrossel-container-player');
+  container.style.transform = `translateX(-${index1 * 30}%)`;
+}
+

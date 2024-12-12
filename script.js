1d7f2d58ced47ef7e313f8eb377f83b58290295a
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2024-12-12 20:43:14 -0300

    aside atualização

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let index1 = 0;
 const items1 = document.querySelectorAll('.carrossel-item-player');
 const totalItems1 = items1.length; 
 
+let index_legenda = 0;
+const items_legenda = document.querySelectorAll('.carrossel-container-legenda');
+const totalItems_legenda = items_legenda.length; 
+
 // Função para mudar a imagem
 function mudarImagem(direcao) {
   // Atualiza o índice com base na direção
@@ -14,6 +18,15 @@ function mudarImagem(direcao) {
   // Move o carrossel para a posição da imagem desejada
   const container = document.querySelector('.carrossel-container-videoclip');
   container.style.transform = `translateX(-${index * 100}%)`;
+
+
+
+  index_legenda = (index_legenda + direcao + totalItems_legenda) % totalItems_legenda;
+
+  const container_legenda = document.querySelector('.carrossel-container-videoclip');
+  container_legenda.style.transform = `translateX(-${index_legenda * 90}%)`;
+
+
 }
 
 function mudarImagem1(direcao) {
@@ -23,4 +36,5 @@ function mudarImagem1(direcao) {
   const container = document.querySelector('.carrossel-container-player');
   container.style.transform = `translateX(-${index1 * 30}%)`;
 }
+
 
